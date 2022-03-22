452cc8b2478256f5686141b6162b40e82a1fc8cf
waltzHao <waltzhao828@gmail.com>
2022-03-22 15:45:10 +0800

    fix deploy config

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,13 @@
 set -e
 # build
 npm run build
-
 cd dist
 
 git init
-git checkout -b main
 git add -A
 git commit -m 'deploy'
 
 
-git push -f git@github.com:waltzHao/verification-code.git main:gh-pages
+git push -f git@github.com:waltzHao/verification-code.git master:gh-pages
 
 cd -
