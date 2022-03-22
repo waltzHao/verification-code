c49d07455410de1832a50e6732288df04f76bbd8
waltzHao <waltzhao828@gmail.com>
2022-03-22 14:58:45 +0800

    deploy configuration

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+# build
+npm run build
+
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+
+git push -f git@github.com:waltzHao/.git main:gh-pages
+
+cd -
